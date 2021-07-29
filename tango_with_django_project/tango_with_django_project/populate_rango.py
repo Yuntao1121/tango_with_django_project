22234511760b0fbabcef15cd870bcf63cfc18973
Yuntao1121 <2512207c@student.gla.ac.uk>
2021-07-30 01:51:43 +0800

    chapter5-Exercises

--- tango_with_django_project/tango_with_django_project/populate_rango.py
+++ tango_with_django_project/tango_with_django_project/populate_rango.py
@@ -1,5 +1,5 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','tango_with_django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
 
 import django
 django.setup()
@@ -7,36 +7,36 @@ from rango.models import Category, Page
 
 def populate():
     python_pages = [
-         {'title': 'Official Python Tutorial', 
+        {'title': 'Official Python Tutorial',
          'url':'http://docs.python.org/3/tutorial/'},
-         {'title':'How to Think like a Computer Scientist',
+        {'title':'How to Think like a Computer Scientist',
          'url':'http://www.greenteapress.com/thinkpython/'},
-         {'title':'Learn Python in 10 Minutes',
+        {'title':'Learn Python in 10 Minutes',
          'url':'http://www.korokithakis.net/tutorials/python/'} ]
     
     django_pages = [
-         {'title':'Official Django Tutorial',
+        {'title':'Official Django Tutorial',
          'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-         {'title':'Django Rocks',
+        {'title':'Django Rocks',
          'url':'http://www.djangorocks.com/'},
-         {'title':'How to Tango with Django',
+        {'title':'How to Tango with Django',
          'url':'http://www.tangowithdjango.com/'} ]
-
+    
     other_pages = [
-         {'title':'Bottle',
+        {'title':'Bottle',
          'url':'http://bottlepy.org/docs/dev/'},
-         {'title':'Flask',
+        {'title':'Flask',
          'url':'http://flask.pocoo.org'} ]
-
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages} }
-
+    
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
+    
     for cat, cat_data in cats.items():
-          c = add_cat(cat) 
-          for p in cat_data['pages']:
-              add_page(c, p['title'], p['url'])
-
+        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
+        for p in cat_data['pages']:
+            add_page(c, p['title'], p['url'])
+    
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
@@ -48,11 +48,12 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
- 
 
 if __name__ == '__main__':
     print('Starting Rango population script...')
--- tango_with_django_project/tango_with_django_project/rango/models.py
+++ tango_with_django_project/tango_with_django_project/rango/models.py
@@ -2,17 +2,21 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    class Meta:
-        verbose_name_plural = 'Categories'
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
     
     def __str__(self):
-        return self.name
+            return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+       
 
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
